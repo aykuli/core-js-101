c9898f2e0deca66d1b4d4cb56bf31bb6cdb1f9fe
Aykuli <aykuli@ya.ru>
2019-12-10 21:48:16 +0700

    feat: 64 passing/37 pending all objects tasks done

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -35,8 +35,8 @@ function Rectangle(width, height) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 /**
@@ -50,8 +50,8 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  return Object.setPrototypeOf(JSON.parse(json), proto);
 }
 
 /**
@@ -106,36 +106,84 @@ function fromJSON(/* proto, json */) {
  *    => 'div#main.container.draggable + table#data ~ tr:nth-of-type(even)   td:nth-of-type(even)'
  *
  *  For more examples see unit tests.
+ * https://www.codewars.com/kata/css-selectors-builder/train/javascript
  */
+class CssSelector {
+  constructor() {
+    this.selector = '';
+    this.type = 0;
+    this.uniqueError = 'Element, id and pseudo-element should not occur more then one time inside the selector'; // eslint-disable-line
+    this.orderError = // eslint-disable-line
+      'Selector parts should be arranged in the following order: ' + // eslint-disable-line
+      'element, id, class, attribute, pseudo-class, pseudo-element'; // eslint-disable-line
+  }
+
+  element(value) {
+    this.checkType(1);
+    this.selector += value;
+    return this;
+  }
+
+  id(value) {
+    this.checkType(2);
+    this.selector += `#${value}`;
+    return this;
+  }
+
+  class(value) {
+    this.checkType(3);
+    this.selector += `.${value}`;
+    return this;
+  }
+
+  attr(value) {
+    this.checkType(4);
+    this.selector += `[${value}]`;
+    return this;
+  }
+
+  pseudoClass(value) {
+    this.checkType(5);
+    this.selector += `:${value}`;
+    return this;
+  }
+
+  pseudoElement(value) {
+    this.checkType(6);
+    this.selector += `::${value}`;
+    return this;
+  }
+
+  combine(selector1, combinator, selector2) {
+    this.selector = `${selector1.stringify()} ${combinator} ${selector2.stringify()}`;
+    return this;
+  }
+
+  stringify() {
+    return this.selector;
+  }
+
+  checkType(type) {
+    if (type < this.type) throw this.orderError;
+    if (type === this.type && [1, 2, 6].includes(type)) throw this.uniqueError;
+    this.type = type;
+  }
+}
 
 const cssSelectorBuilder = {
-  element(/* value */) {
-    throw new Error('Not implemented');
-  },
-
-  id(/* value */) {
-    throw new Error('Not implemented');
-  },
-
-  class(/* value */) {
-    throw new Error('Not implemented');
-  },
-
-  attr(/* value */) {
-    throw new Error('Not implemented');
-  },
-
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
-  },
-
-  pseudoElement(/* value */) {
-    throw new Error('Not implemented');
-  },
-
-  combine(/* selector1, combinator, selector2 */) {
-    throw new Error('Not implemented');
-  },
+  // prettier-ignore
+  element: (value) => new CssSelector().element(value),
+  // prettier-ignore
+  id: (value) => new CssSelector().id(value),
+  // prettier-ignore
+  class: (value) => new CssSelector().class(value),
+  // prettier-ignore
+  attr: (value) => new CssSelector().attr(value),
+  // prettier-ignore
+  pseudoClass: (value) => new CssSelector().pseudoClass(value),
+  // prettier-ignore
+  pseudoElement: (value) => new CssSelector().pseudoElement(value),
+  combine: (s1, c, s2) => new CssSelector().combine(s1, c, s2),
 };
 
 module.exports = {
